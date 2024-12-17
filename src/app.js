9d56fd8c1df5423bf28cdd9b8f8b9093536e28ac
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2024-12-17 22:24:41 +0000

    DomainMartin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import "./index.html";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    let a = pronoun[i];
+
+    for (let j = 0; j < adj.length; j++) {
+      let b = adj[j];
+
+      for (let k = 0; k < noun.length; k++) {
+        let c = noun[k];
+        console.log(a + b + c + ".com");
+      }
+    }
+  }
 };
